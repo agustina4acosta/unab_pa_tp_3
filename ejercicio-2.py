d48e9207454d3aae80a17c43802cee6b839bbaa5
agustina4acosta <agustinaderek09@gmail.com>
2026-04-06 20:09:17 -0300

    Ejercicio 2 / clase Punto

---
+++ ejercicio-2.py
@@ -0,0 +1,13 @@
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def eje_x(self):
+        return self.x
+    def eje_y(self):
+        return self.y
+    def impresion(self):
+        return f"Punto({self.x}, {self.y})"
+    def opuesto(self):
+        return Punto(-self.x, -self.y)
+    
