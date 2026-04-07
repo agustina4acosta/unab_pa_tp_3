555b85db57e01aaa1e3dda2306d402e38875fc5d
agustina4acosta <agustinaderek09@gmail.com>
2026-04-06 21:22:04 -0300

    ejericicio 4

--- ejercicio_4.py
+++ ejercicio_4.py
@@ -2,3 +2,11 @@ class Cancion:
     def __init__(self, titulo, autor):
         self.titulo = titulo
         self.autor = autor
+    def get_titulo(self):
+        return self.titulo
+    def get_autor(self):
+        return self.autor
+    def set_titulo(self, nuevo_titulo):
+        self.titulo = nuevo_titulo
+    def set_autor(self, nuevo_autor):
+        self.autor = nuevo_autor
